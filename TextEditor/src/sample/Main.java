b97e91196873b245924c85cde43b3acd6e0307ee
RedCrabVb <cany245@mail.ru>
2021-09-09 17:11:41 +0300

    Added: font changes, color changes, default values Corrected: removed unnecessary code, removed unnecessary comments

--- TextEditor/src/sample/Main.java
+++ TextEditor/src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
@@ -15,66 +16,16 @@ import javafx.scene.text.Font;
 
 import java.util.stream.Collectors;
 
-class CreateLabeled {
-    static Button createButton(int posX, int posY, int prefSizeX, int prefSizeY, String text) {
-        Button obj = new Button();
-        obj.setText(text);
-        obj.setTranslateX(posX);
-        obj.setTranslateY(posY);
-        obj.setPrefSize(prefSizeX, prefSizeY);
-        return obj;
-    }
-
-    static Label createLabel(int posX, int posY, int prefSizeX, int prefSizeY, String text) {
-        Label obj = new Label();
-        obj.setText(text);
-        obj.setTranslateX(posX);
-        obj.setTranslateY(posY);
-        obj.setPrefSize(prefSizeX, prefSizeY);
-        return obj;
-    }
-
-    static RadioButton createRadioButton (int posX, int posY, int prefSizeX, int prefSizeY, String text) {
-        RadioButton obj = new RadioButton();
-        obj.setText(text);
-        obj.setTranslateX(posX);
-        obj.setTranslateY(posY);
-        obj.setPrefSize(prefSizeX, prefSizeY);
-        return obj;
-    }
-
-    static CheckBox createCheckBox (int posX, int posY, int prefSizeX, int prefSizeY, String text) {
-        CheckBox obj = new CheckBox();
-        obj.setText(text);
-        obj.setTranslateX(posX);
-        obj.setTranslateY(posY);
-        obj.setPrefSize(prefSizeX, prefSizeY);
-        return obj;
-    }
-}
-
-
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-/*        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();*/
-
         primaryStage.setTitle("TextEditor");
         primaryStage.setWidth(400);
         primaryStage.setHeight(400);
 
         Pane root = new Pane();
-
-        Button button = new Button();
-        button.setText("Load");
-        button.setTranslateX(20);
-        button.setTranslateY(300);
-        button.setPrefSize(60, 60);
 
         Label label = new Label("Text");
         label.setTranslateX(20);
@@ -83,47 +34,52 @@ public class Main extends Application {
         TextField textField = new TextField();
         textField.setTranslateX(20);
         textField.setTranslateY(50);
+        textField.setFont(Font.font(Font.getFontNames().stream().findFirst().get(), 14));
 
 
+        int posCombatY = 120;
         ComboBox<String> comboBoxFonts = new ComboBox<>();
         comboBoxFonts.setTranslateX(20);
-        comboBoxFonts.setTranslateY(130);
-        comboBoxFonts.getItems().addAll(Font.getFontNames().stream().limit(10).collect(Collectors.toList()));
-
-        comboBoxFonts.setOnAction(actionEvent -> {
-            textField.setFont(Font.font(comboBoxFonts.getValue()));
-        });
+        comboBoxFonts.setTranslateY(posCombatY);
+        comboBoxFonts.getItems().addAll(Font.getFontNames().stream().limit(30).collect(Collectors.toList()));
+        comboBoxFonts.setValue(comboBoxFonts.getItems().get(0));
 
         ComboBox<Integer> comboBoxFontSize = new ComboBox<>();
         comboBoxFontSize.setTranslateX(20);
-        comboBoxFontSize.setTranslateY(170);
+        comboBoxFontSize.setTranslateY((posCombatY = posCombatY + 40));
         for (int i = 8; i < 30; i++) {
             comboBoxFontSize.getItems().add(i);
         }
-        comboBoxFontSize.setOnAction(actionEvent -> {
-            textField.setFont(Font.font(comboBoxFonts.getValue(), comboBoxFontSize.getValue()));
-        });
+        comboBoxFontSize.setValue(comboBoxFontSize.getItems().get(6));
 
-        ComboBox<Color> comboBoxColors = new ComboBox<>();
+        ComboBox<String> comboBoxColors = new ComboBox<>();
         comboBoxColors.setTranslateX(20);
-        comboBoxColors.setTranslateY(200);
-        comboBoxColors.getItems().addAll(Color.RED, Color.BLACK, Color.BLUE, Color.GRAY);
+        comboBoxColors.setTranslateY((posCombatY = posCombatY + 40));
+        comboBoxColors.getItems().addAll("black", "red", "blue", "gray");
+        comboBoxColors.setValue(comboBoxColors.getItems().get(0));
+
+        ComboBox<FontWeight> comboBoxWight = new ComboBox<>();
+        comboBoxWight.setTranslateX(20);
+        comboBoxWight.setTranslateY((posCombatY = posCombatY + 40));
+        comboBoxWight.getItems().addAll(FontWeight.NORMAL, FontWeight.BLACK, FontWeight.LIGHT, FontWeight.SEMI_BOLD, FontWeight.EXTRA_BOLD);
+        comboBoxWight.setValue(comboBoxWight.getItems().get(0));
+
         comboBoxColors.setOnAction(actionEvent -> {
-            textField.setFont(Font.font(comboBoxFonts.getValue(), comboBoxFontSize.getValue()));
-            textField.setStyle("-fx-text-inner-color: red");
+            textField.setStyle(String.format("-fx-text-inner-color: %s", comboBoxColors.getValue()));
+        });
+        comboBoxFonts.setOnAction(actionEvent -> {
+            textField.setFont(Font.font(comboBoxFonts.getValue(), comboBoxWight.getValue(), comboBoxFontSize.getValue()));
+        });
+        comboBoxFontSize.setOnAction(actionEvent -> {
+            textField.setFont(Font.font(comboBoxFonts.getValue(), comboBoxWight.getValue(), comboBoxFontSize.getValue()));
+        });
+        comboBoxWight.setOnAction(actionEvent -> {
+            textField.setFont(Font.font(comboBoxFonts.getValue(), comboBoxWight.getValue(), comboBoxFontSize.getValue()));
         });
 
-
-
-//        group.getChildren().addAll(button1, button2);
 
         Scene scene = new Scene(root);
-        root.getChildren().addAll(button);
-        root.getChildren().addAll(label);
-        root.getChildren().add(textField);
-        root.getChildren().add(comboBoxFonts);
-        root.getChildren().add(comboBoxFontSize);
-        root.getChildren().add(comboBoxColors);
+        root.getChildren().addAll(label, textField, comboBoxFonts, comboBoxFontSize, comboBoxWight, comboBoxColors);
         primaryStage.setScene(scene);
         primaryStage.show();
 
